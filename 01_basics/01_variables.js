3a172963ccdabdd45116927d37a3ffdda35411e4
samsul7718 <108485240+Samsul7718@users.noreply.github.com>
2023-11-03 19:40:35 +0000

    open new folder

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144255
+let accountEmail="samsul7718@gmail.com"
+var accountPassword="12345"
+accountCity="hyderabad"
+
+accountEmail="sam@gmail.com"
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
