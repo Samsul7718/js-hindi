6ce9daff07bbd7183680f8ad43ef7745d3e59424
samsul7718 <108485240+Samsul7718@users.noreply.github.com>
2023-11-07 19:52:45 +0000

    nums and math

---
+++ 01_basics/03_nums_and_math.js
@@ -0,0 +1,29 @@
+const score=400
+// console.log(score);
+
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+
+const otherNumber=123.869
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds=1000000;
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+// ********************maths********************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(6,2,1,3,8));
+// console.log(Math.max(6,2,1,3,8));
+
+// console.log(Math.random());
+const min =10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
