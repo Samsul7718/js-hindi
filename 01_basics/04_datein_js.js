2c6bc0bf5d934a5b93ab4db0a7e9b2e7c6f1b099
samsul7718 <108485240+Samsul7718@users.noreply.github.com>
2023-11-08 13:30:52 +0000

    datein js

---
+++ 01_basics/04_datein_js.js
@@ -0,0 +1,12 @@
+let myDate= new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString)
+// console.log(typeof myDate)
+
+// let myCreatedDate= new Date(2023, 10, 8)
+let myCreatedDate= new Date(2023, 10, 8, 12,20)
+// console.log(myCreatedDate.toLocaleString());
+let myTimeStamp= Date.now()
+// console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000));
