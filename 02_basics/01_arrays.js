799d74399ee092dfadd3f6a8131517e898a2b82f
samsul7718 <108485240+Samsul7718@users.noreply.github.com>
2023-11-08 16:40:13 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+// array
+const myARR= [0,1,2,3,4,5]
+console.log(myARR[2]);
+
+// Array methods
+myARR.push(6)
+myARR.push(7)
+console.log(myARR);
+myARR.pop()
+console.log(myARR);
+
+// sclice splice
+console.log("A", myARR);
+const myn1=myARR.slice(1,3)
+console.log(myn1);
+console.log("B", myARR);
