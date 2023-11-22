d6d96b8a529b1932bf37c86cb561fafbaa2a9f26
samsul7718 <108485240+Samsul7718@users.noreply.github.com>
2023-11-22 09:37:54 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,12 @@
+function sayMyName(){
+    console.log("s");
+    console.log("a");
+    console.log("m");
+}
+// sayMyName()
+function addTwoNum(num1, num2){
+   let result=num1+num2;
+   return result
+}
+const result = addTwoNum(1,2)
+console.log("Result :", result);
