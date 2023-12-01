51a39193ffd1ebc97f59d48045172a5d425b7cee
samsul7718 <108485240+Samsul7718@users.noreply.github.com>
2023-12-01 01:29:44 +0000

    commit

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,4 +9,26 @@ function addTwoNum(num1, num2){
    return result
 }
 const result = addTwoNum(1,2)
-console.log("Result :", result);
+// console.log("Result :", result);
+
+function addCartPrice(...num1){
+   return num1
+}
+// console.log(addCartPrice(200,300,500))
+const user={
+ username:"samsul",
+ price:199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username:"samsul",
+    price:299
+})
+const myNewArray=[200,400,600,800]
+function returnValue(getArray){
+    return getArray[1]
+}
+console.log(returnValue(myNewArray))
