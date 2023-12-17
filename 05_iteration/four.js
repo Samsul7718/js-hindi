09556804226e85df9ee5e9c0b458d1d734489a39
samsul7718 <108485240+Samsul7718@users.noreply.github.com>
2023-12-17 05:23:16 +0000

    new comment

---
+++ 05_iteration/four.js
@@ -0,0 +1,25 @@
+const myObject={
+    js:"javascript",
+    cpp:"c++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+for (const key in myObject) {
+//    console.log(`${key} short form of value ${myObject[key]}`);
+}
+
+const programming=["js","cpp","java","ruby"]
+
+// for (const key in programming) {
+//   console.log(programming[key]);
+// }
+
+  // Map
+//   const map= new Map()
+//   map.set('In',"india")
+//   map.set('USA',"united state of america")
+//   map.set('Fr',"france")
+
+//   for (const key in map) {
+//     console.log(key);
+//   }
